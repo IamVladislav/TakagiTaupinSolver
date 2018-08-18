d5788620103ac7a46e3000f8eb312ebe4065f8f5
Vladislav Metel <metel.vlad@gmail.com>
2018-08-18 14:29:48 +0300

    Some bones of future programs

--- TakagiTaupinSolver/main.cpp
+++ TakagiTaupinSolver/main.cpp
@@ -7,9 +7,191 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+class SystemReplacment{
+public:
+    double xc, yc, zc;
+    double xcinv, ycinv, zcinv;
+};
+
+class RotationMatrixZ: public SystemReplacment{
+public:
+    double phi;
+    RotationMatrixZ(double phi){
+        this->phi=phi;
+    }
+    /*double xc(double x, double y, double z){
+        double xcv;
+        xcv=x*cos(phi);
+        return xcv;
+    }*/
+    void Basis(double x, double y, double z){//mock
+        xc=3;
+        yc=3;
+        zc=5;
+    }
+};
+
+
+class DisplacmentGradient{
+public:
+    double uxx,uxy,uxz,uyx,uyy,uyz,uzx,uzy,uzz;
+    virtual double uxxcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uxycalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uxzcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uyxcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uyycalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uyzcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uzxcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uzycalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+    virtual double uzzcalc(double x, double y, double z)
+    {
+        return uxz;
+    }
+};
+
+class TestDisp: public DisplacmentGradient{
+public:
+    double uxxcalc(double x, double y, double z) override
+    {
+        uxx=10;
+        return uxx;
+    }
+    double uxycalc(double x, double y, double z) override
+    {
+        uxy=5;
+        return uxy;
+    }
+    double uxzcalc(double x, double y, double z) override
+    {
+        uxz=5;
+        return uxz;
+    }
+    double uyxcalc(double x, double y, double z) override
+    {
+        uyx=5;
+        return uyx;
+    }
+    double uyycalc(double x, double y, double z) override
+    {
+        uyy=5;
+        return uyy;
+    }
+    double uyzcalc(double x, double y, double z) override
+    {
+        uyy=5;
+        return uyz;
+    }
+    double uzxcalc(double x, double y, double z) override
+    {
+        uzx=5;
+        return uzx;
+    }
+    double uzycalc(double x, double y, double z) override
+    {
+        uzy=5;
+        return uzy;
+    }
+    double uzzcalc(double x, double y, double z) override
+    {
+        uzz=5;
+        return uzz;
+    }
+};
+
+class DisplacmentGradientSystemReplace: public virtual DisplacmentGradient{
+public:
+    DisplacmentGradient displacment;
+    RotationMatrixZ *rotate;
+    double l;//mock
+    DisplacmentGradientSystemReplace(double bu, double h, double nu, DisplacmentGradient obj)//mock
+    {
+        displacment=obj;
+        rotate = new RotationMatrixZ(l);
+        std::cout<<obj.uxx<<std::endl;
+        std::cout<<displacment.uxx<<std::endl;
+        std::cout<<obj.uxxcalc(1, 1, 1)<<std::endl;
+    }
+    double uxxcalc(double x, double y, double z) override
+    {
+        rotate->Basis(x,y,z);
+        uxx=displacment.uxxcalc(rotate->xc, rotate->yc, rotate->zc)*cos(l)*cos(l) - displacment.uxycalc(rotate->xc, rotate->yc, rotate->zc)*cos(l)*sin(l) - displacment.uyxcalc(rotate->xc, rotate->yc, rotate->zc)*cos(l)*sin(l) + displacment.uyycalc(rotate->xc, rotate->yc, rotate->zc)*sin(l)*sin(l);
+        return uxx;
+    }
+    double uxycalc(double x, double y, double z) override
+    {
+        uxy=displacment.uxxcalc(rotate->xc, rotate->yc, rotate->zc);//mock
+        return uxy;
+    }
+    double uxzcalc(double x, double y, double z) override
+    {
+        uxz=5;
+        return uxz;
+    }
+    double uyxcalc(double x, double y, double z) override
+    {
+        uyx=5;
+        return uyx;
+    }
+    double uyycalc(double x, double y, double z) override
+    {
+        uyy=5;
+        return uyy;
+    }
+    double uyzcalc(double x, double y, double z) override
+    {
+        uyz=5;
+        return uyz;
+    }
+    double uzxcalc(double x, double y, double z) override
+    {
+        uzx=5;
+        return uzx;
+    }
+    double uzycalc(double x, double y, double z) override
+    {
+        uzy=5;
+        return uzy;
+    }
+    double uzzcalc(double x, double y, double z) override
+    {
+        uzz=5;
+        return uzz;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    TestDisp p;
+    p.uxxcalc(1, 1, 1);
+    //std::cout << p.uxx << std::endl;
+    RotationMatrixZ t(10);
+    //std::cout<<t.Test()<<std::endl;
+    DisplacmentGradientSystemReplace obj(1, 1, 1, p);
+    //std::cout<<obj.uxycalc(1, 1, 1)<<std::endl;
     return 0;
 }
