168cb778a1b7336b6fb9b1e3de6b26163c7c3cff
Vladislav Metel <vladislavmetel@msk-wifi-25fap4-pmiroshnichenko15-p.miroshnichenko.mail.msk>
2018-09-01 12:13:36 +0300

    Add a separate file for System_Replacement.

---
+++ TakagiTaupinSolver/System_Repclacement.hpp
@@ -0,0 +1,14 @@
+//
+//  System_Repclacement.hpp
+//  TakagiTaupinSolver
+//
+//  Created by Vladislav Metel on 01/09/2018.
+//  Copyright © 2018 Владислав Метель. All rights reserved.
+//
+
+#ifndef System_Repclacement_hpp
+#define System_Repclacement_hpp
+
+#include <stdio.h>
+
+#endif /* System_Repclacement_hpp */
