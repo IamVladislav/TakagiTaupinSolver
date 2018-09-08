83e8e2f70f387d026c544a993f358aa22041de7b
Vladislav Metel <metel.vlad@gmail.com>
2018-09-08 22:07:21 +0300

    Fix y_rotation like in Testing_Test

--- TakagiTaupinSolver/Distorsion_Source.cpp
+++ TakagiTaupinSolver/Distorsion_Source.cpp
@@ -15,9 +15,9 @@ BeamDislocation::BeamDislocation(double nu, double kappa, double dislocation_dep
     this->depth=dislocation_depth;
     theta=PI_2-kappa;
     parallel_shift = new Parallel_Shfit(-(sqrt(pow(exit_point_coordinate.c[0],2)+pow(exit_point_coordinate.c[1],2))+segment_lenght), 0, exit_point_coordinate.c[2]);
-    Rotation_Matrix_Y_Vector *burgers_vector_rotate = new Rotation_Matrix_Y_Vector(theta);
-    burgers_vector_rotate->Basis(burgers_vector);
-    this->burgers_vector=burgers_vector_rotate->vector;
+    Rotation_Matrix_Y_Vector *burgers_vector_rotate = new Rotation_Matrix_Y_Vector(PI_2-theta);
+    burgers_vector_rotate->Basis_Inverse(burgers_vector);
+    this->burgers_vector=burgers_vector_rotate->vector_inv;
 }
 double BeamDislocation::uxxcalc(double x, double y, double z){
     parallel_shift->Basis(x, y, z);
