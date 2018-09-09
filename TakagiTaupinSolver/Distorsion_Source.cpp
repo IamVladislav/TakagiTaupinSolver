984a524a7d1df0dc674614d73f29b34a42c66b54
Vladislav Metel <vladislavmetel@vladislavs-macbook-pro.local>
2018-09-09 22:33:36 +0300

    Fix segment length into distortion fields.

--- TakagiTaupinSolver/Distorsion_Source.cpp
+++ TakagiTaupinSolver/Distorsion_Source.cpp
@@ -14,7 +14,7 @@ BeamDislocation::BeamDislocation(double nu, double kappa, double dislocation_dep
     this->segment_lenght=segment_lenght;
     this->depth=dislocation_depth;
     theta=PI_2-kappa;
-    parallel_shift = new Parallel_Shfit(-(sqrt(pow(exit_point_coordinate.c[0],2)+pow(exit_point_coordinate.c[1],2))+segment_lenght), 0, exit_point_coordinate.c[2]);
+    parallel_shift = new Parallel_Shfit(-(sqrt(pow(exit_point_coordinate.c[0],2)+pow(exit_point_coordinate.c[1],2))), 0, exit_point_coordinate.c[2]);
     Rotation_Matrix_Y_Vector *burgers_vector_rotate = new Rotation_Matrix_Y_Vector(PI_2-theta);
     burgers_vector_rotate->Basis_Inverse(burgers_vector);
     this->burgers_vector=burgers_vector_rotate->vector_inv;
@@ -503,7 +503,7 @@ AngularDislocation::AngularDislocation(double nu, double kappa, double dislocati
     this->segment_lenght=segment_lenght;
     this->burgers_vector=burgers_vector;
     this->depth=dislocation_depth;
-    parallel_shift = new Parallel_Shfit(segment_lenght, 0, 0);
+    parallel_shift = new Parallel_Shfit(-segment_lenght, 0, 0);
     }
 double AngularDislocation::uxxcalc(double x, double y, double z){
     parallel_shift->Basis(x, y, z);
